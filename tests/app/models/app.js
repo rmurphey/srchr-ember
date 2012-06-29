eb4650cdf210480f23f750e48ddb980854ae26cd
Rebecca Murphey <rmurphey@gmail.com>
2012-06-28 20:13:20 -0700

    remove backbone tests

--- tests/app/models/app.js
+++
@@ -1,9 +0,0 @@
-define([
-  'app/models/app'
-], function(app, undef) {
-  describe("App model", function() {
-    it("should have a currentSearch property", function() {
-     expect(app.get('currentSearch')).not.to.be(undef);
-    });
-  });
-});
